e636061d7dfb496bd0b5c91295d6773ba69cfdcc
alex <argriffi@ncsu.edu>
2014-05-08 15:10:54 -0400

    add more details to the example

--- examples/geneconv/geneconv.py
+++ examples/geneconv/geneconv.py
@@ -20,6 +20,7 @@ from npmctree.dynamic_fset_lhood import get_lhood
 
 
 def get_data_constraints(nodes, pair_to_state):
+    # constructed with branch length 0.1 for all branches and phi = 2.0.
     nsites = 60
     leaf_to_paralogs = {
             'N0' : (
@@ -57,9 +58,7 @@ def get_data_constraints(nodes, pair_to_state):
                 fset[state] = 1
                 node_to_data_fset[node] = fset
         per_site_constraints.append(node_to_data_fset)
-    print('paralog site matches   :', npaired_yes)
-    print('paralog site mismatches:', npaired_no)
-    return per_site_constraints
+    return per_site_constraints, npaired_yes, npaired_no
 
 
 def get_tree_info():
@@ -67,11 +66,11 @@ def get_tree_info():
     edge_to_blen = {}
     root = 'N1'
     triples = (
-            ('N1', 'N0', 0.1),
-            ('N1', 'N2', 0.1),
-            ('N1', 'N5', 0.1),
-            ('N2', 'N3', 0.1),
-            ('N2', 'N4', 0.1))
+            ('N1', 'N0', 0.4),
+            ('N1', 'N2', 0.4),
+            ('N1', 'N5', 0.4),
+            ('N2', 'N3', 0.4),
+            ('N2', 'N4', 0.4))
     for va, vb, blen in triples:
         edge = (va, vb)
         T.add_edge(*edge)
@@ -178,8 +177,102 @@ def objective(T, root, edge_to_blen, nt_pairs, constraints, params):
     return neg_ll
 
 
-
 def main(args):
+    """
+    Sample some data and subsequently infer parameter values.
+
+    """
+    # define the state space
+    nt_pairs = []
+    pair_to_state = {}
+    for i, pair in enumerate(product('acgt', repeat=2)):
+        nt_pairs.append(pair)
+        pair_to_state[pair] = i
+    nstates = len(nt_pairs)
+
+    # interpret the command line arguments
+    nhistories = args.n
+    phi = args.phi
+
+    # get the transition rate matrix and stationary distribution
+    Q, root_distn = get_Q_and_distn(nt_pairs, phi)
+
+    # get the tree info
+    T, root, edge_to_blen = get_tree_info()
+    nodes = set(T)
+    leaves = set(v for v, degree in T.degree().items() if degree == 1)
+    not_leaves = nodes - leaves
+
+    # compute probability transition matrices for each edge
+    edge_to_P = {}
+    for edge in T.edges():
+        blen = edge_to_blen[edge]
+        P = scipy.linalg.expm(blen * Q)
+        edge_to_P[edge] = P
+
+    # do not impose any data constraints
+    node_to_data_lmap = {}
+    for node in nodes:
+        node_to_data_lmap[node] = np.ones(nstates)
+
+    # for each node, get a sequence of nucleotide pairs
+    npaired_yes = 0
+    npaired_no = 0
+    constraints = []
+    node_to_pairs = dict((node, []) for node in nodes)
+    for node_to_state in sample_histories(T, edge_to_P, root,
+            root_distn, node_to_data_lmap, nhistories):
+
+        # convert node_to_state to constraints
+        d = {}
+        for v in not_leaves:
+            d[v] = np.ones(nstates, dtype=bool)
+        for v, state in node_to_state.items():
+            d[v] = np.zeros(nstates, dtype=bool)
+            d[v][state] = True
+        constraints.append(d)
+        
+        # summarize the data and build the sequences from the sites
+        for node, state in node_to_state.items():
+            nt_pair = nt_pairs[state]
+            if nt_pair[0] == nt_pair[1]:
+                npaired_yes += 1
+            else:
+                npaired_no += 1
+            node_to_pairs[node].append(nt_pair)
+
+    # report the paralogs for the leaf nodes
+    for leaf in leaves:
+        pairs = node_to_pairs[leaf]
+        seqs = [''.join(x).upper() for x in zip(*pairs)]
+        print('taxon', leaf)
+        print('paralog 1:', seqs[0])
+        print('paralog 2:', seqs[1])
+        print()
+
+    # Get the negative log likelihood function,
+    # purely as a function of the phi parameter.
+    f = partial(objective, T, root, edge_to_blen, nt_pairs, constraints)
+
+    # Use the data summary to get a preliminary estimate of phi.
+    # This would be the correct estimate if branch lengths were zero,
+    # using a back of the envelope calculation.
+    print('paralog site matches   :', npaired_yes)
+    print('paralog site mismatches:', npaired_no)
+    nsmall = 4
+    pa = npaired_yes * (nsmall - 1)
+    pb = npaired_no
+    phi_hat = pa / pb
+    print('preliminary estimate of phi:', phi_hat)
+
+    # get the max likelihood estimate of phi
+    result = scipy.optimize.minimize(
+            f, x0=[phi_hat], method='L-BFGS-B', bounds=[(0, None)])
+    print(result)
+
+
+
+def main_inference(args):
 
     # define the state space
     nt_pairs = []
@@ -199,12 +292,27 @@ def main(args):
     leaves = set(v for v, degree in T.degree().items() if degree == 1)
 
     # get the per-site constraints
-    constraints = get_data_constraints(nodes, pair_to_state)
+    constraint_info = get_data_constraints(nodes, pair_to_state)
+    constraints, npaired_yes, npaired_no = constraint_info
+
+    # Get the negative log likelihood function,
+    # purely as a function of the phi parameter.
+    f = partial(objective, T, root, edge_to_blen, nt_pairs, constraints)
+
+    # Use the data summary to get a preliminary estimate of phi.
+    # This would be the correct estimate if branch lengths were zero,
+    # using a back of the envelope calculation.
+    print('paralog site matches   :', npaired_yes)
+    print('paralog site mismatches:', npaired_no)
+    nsmall = 4
+    pa = npaired_yes * (nsmall - 1)
+    pb = npaired_no
+    phi_hat = pa / pb
+    print('preliminary estimate of phi:', phi_hat)
 
     # get the max likelihood estimate of phi
-    f = partial(objective, T, root, edge_to_blen, nt_pairs, constraints)
     result = scipy.optimize.minimize(
-            f, x0=[3.0], method='L-BFGS-B', bounds=[(0, None)])
+            f, x0=[phi_hat], method='L-BFGS-B', bounds=[(0, None)])
     print(result)
 
 
