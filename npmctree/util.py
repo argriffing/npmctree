b269e4afceb2b065ba2c70ca67a2ddd8e43b173f
alex <argriffi@ncsu.edu>
2014-07-22 14:06:48 -0400

    add a helper function for converting from a data format of lower generality to a format of higher generality

--- npmctree/util.py
+++ npmctree/util.py
@@ -56,3 +56,33 @@ def ddec(**kwargs):
         return obj
     return dec
 
+
+def xmap_to_lmap(all_nodes, nstates, xmap, validate=True):
+    """
+    Convert from a more restricted to a less restricted observation format.
+
+    Use of this function is a likely indicator of inefficiency
+    in the calling function.
+    For greater efficiency the caller should use the xmap format directly.
+
+    """
+    all_nodes = set(all_nodes)
+    observed_nodes = set(xmap)
+    hidden_nodes = all_nodes - observed_nodes
+    if validate:
+        extra_nodes = observed_nodes - all_nodes
+        if extra_nodes:
+            raise ValueError('extra nodes: %s' % extra_nodes)
+        for state in xmap.values():
+            if int(state) != state:
+                raise ValueError('expected integer state')
+            if not (0 <= state < nstates):
+                raise ValueError('state is out of bounds')
+    node_to_lmap = {}
+    for node, state in xmap.items():
+        lmap = np.zeros(nstates, dtype=float)
+        lmap[state] = 1
+        node_to_lmap[node] = lmap
+    for node in hidden_nodes:
+        node_to_lmap[node] = np.ones(nstates, dtype=float)
+    return node_to_lmap
